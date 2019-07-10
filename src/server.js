2d407d3de890bb8fa319ad3e8b6d192ccf3b4409
blitz450 <saurabh.chauhan450@gmail.com>
2019-07-11 00:35:49 +0530

    Skeleton/Functionality

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const testRoutes = require('../routes/testRoutes');
+// const socketio = require('socket.io');
 
 const app = express();
+// const server = http.createServer(app);
+// const io = socketio(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.listen(PORT, () => {
+app.use('/test', testRoutes);
+
+// io.on('connection', socket => {
+//   console.log('New WebSocket connection');
+
+//   socket.emit('reportAdded', notif);
+// });
+
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+const io = require('./socket').init(server);
+io.on('connection', socket => {
+  console.log('Client connected');
 });
